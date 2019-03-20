44ea880088414cf16e40ac0e43a50adbde7b448d
Dlee4428 <44447609+Dlee4428@users.noreply.github.com>
2019-03-20 19:19:55 -0400

    Changed ImGui another window open close

--- ComponentFramework3.0/ComponentFramework/Scene0.cpp
+++ ComponentFramework3.0/ComponentFramework/Scene0.cpp
@@ -79,8 +79,8 @@ void Scene0::OnDestroy(){
 void Scene0::Update(const float deltaTime){
 	//model0->Update(deltaTime);
 
-	bool show_demo_window = false;
-	bool show_another_window = false;
+	static bool show_demo_window = true;
+	static bool show_another_window = true;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 	static float f = 0.0f;
 	static int counter = 0;
@@ -101,7 +101,7 @@ void Scene0::Update(const float deltaTime){
 	}
 
 	ImGui::BulletText("Testing");
-	ImGui::SliderFloat("float", &f, 0.0f, 500.0f);
+	ImGui::SliderFloat("float", &f, 0.0f, 500.0f); //I need to replace with object translate inside instead of static float value
 
 	ImGui::Checkbox("Demo Window", &show_demo_window);
 	ImGui::Checkbox("Another Window", &show_another_window);
@@ -113,17 +113,23 @@ void Scene0::Update(const float deltaTime){
 	ImGui::Text("Counter = %d", counter);
 
 	// 2. This is another Simple window, let say I want to open separate character editor from main editor
-	// This should work with it.
-	if (!show_another_window) {
-		ImGui::Begin("Another Window", &show_another_window);
-		ImGui::Text("Hello Another window!");
-		if (ImGui::Button("Close Me"))
-			show_another_window = false;
-		ImGui::End();
+	if (show_another_window) {
+		if (!ImGui::Begin("Another Window", &show_another_window))
+		{
+			ImGui::End();
+		} else
+		{
+			ImGui::Text("Hello Another window!");
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+				1000.0f / ImGui::GetIO().Framerate,
+				ImGui::GetIO().Framerate);
+			ImGui::End();
+
+		}
 	}
 
 	// 3. Show the ImGui demo window.
-	if (!show_demo_window) {
+	if (show_demo_window) {
 		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver);
 		ImGui::ShowDemoWindow(&show_demo_window);
 	}
